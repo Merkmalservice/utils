f8827fd8c01de5e612745320b63457870eb03259
maxstolze <maximilian.stolze@researchstudio.at>
2021-06-01 11:38:49 +0200

    Implement MEStringValue, MEIntegerValue,MEBooleanValue & MEFloatValue | update version 0.1.4

--- src/main/java/at/researchstudio/sat/merkmalservice/model/EnumFeature.java
+++ src/main/java/at/researchstudio/sat/merkmalservice/model/EnumFeature.java
@@ -21,13 +21,53 @@ public class EnumFeature extends Feature {
         }
     }
 
-    public static class OptionValue {
+    public static abstract class OptionValue {
         private String value;
         private String description;
 
         public OptionValue(String value, String description) {
             this.value = value;
             this.description = description;
+        }
+
+        public OptionValue(String description) {
+            this.description = description;
+        }
+    }
+
+    public static class MEStringValue extends OptionValue {
+        private String stringValue;
+
+        public MEStringValue(String stringValue, String description) {
+            super(description);
+            this.stringValue = stringValue;
+        }
+    }
+
+    public static class MEIntegerValue extends OptionValue {
+        private Integer integerValue;
+
+        public MEIntegerValue(Integer integerValue, String description) {
+            super(description);
+            this.integerValue = integerValue;
+        }
+    }
+
+    public static class MEFloatValue extends OptionValue {
+        private Float floatValue;
+
+        public MEFloatValue(Float floatValue, String description) {
+            super(description);
+            this.floatValue = floatValue;
+        }
+    }
+
+    public static class MEBooleanValue extends OptionValue {
+        private Boolean booleanValue;
+
+        public MEBooleanValue(Boolean booleanValue, String description) {
+            super(description);
+            this.booleanValue = booleanValue;
         }
     }
 }
