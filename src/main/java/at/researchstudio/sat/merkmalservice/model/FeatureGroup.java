365500c80cc2d14cf145682560ef17499300252e
maxstolze <maximilian.stolze@researchstudio.at>
2021-08-02 17:17:25 +0200

    Add description to FeatureGroup - new Version 0.2.3

--- src/main/java/at/researchstudio/sat/merkmalservice/model/FeatureGroup.java
+++ src/main/java/at/researchstudio/sat/merkmalservice/model/FeatureGroup.java
@@ -2,12 +2,23 @@ package at.researchstudio.sat.merkmalservice.model;
 
 public class FeatureGroup {
     private final String name;
+    private final String description;
 
     public FeatureGroup(String name) {
         this.name = name;
+        this.description = "";
+    }
+
+    public FeatureGroup(String name, String description) {
+        this.name = name;
+        this.description = description;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
